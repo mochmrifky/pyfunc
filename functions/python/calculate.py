cbaf6e2317154229633a54ea6f3d191f93abcdf2
mochmrifky <mochmrifky@tujuhsembilan.com>
2024-08-05 14:59:31 +0700

    remove env

--- functions/python/calculate.py
+++ functions/python/calculate.py
@@ -9,9 +9,7 @@ result = a + b
 # Print the result
 if result >= 1000:
     print("Success calculate a + b =", result)
-    print(os.environ['ENV_VAR'])
     exit(0)
 elif result <= 1000:
     print("Too tiny dude")
-    print(os.environ['ENV_VAR'])
     exit(1)
